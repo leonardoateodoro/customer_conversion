e7cf84a0cda6a71ef328652b8710e78b6a5e8c0c
leonardoateodoro <leonardo.operator@gmail.com>
2022-08-03 22:45:36 -0300

    modificado branch dev app e app.css

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -17,7 +17,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Branch dev
-          <div>teste comment</div>
+          <div>test commentary</div>
         </a>  
       </header>
     </div> 
